6ac09ea7481811f6db6707b4252c007ffef3f559
psy-code115 <jpenaandrade97@gmail.com>
2025-10-17 15:14:02 -0700

    external links and staff

--- server.js
+++ server.js
@@ -35,20 +35,73 @@ const extLinks = [
   },
 ];
 
+const staff = [
+  { id: 'john',  
+    name: 'John Doe',  
+    position: 'game designer',            
+    email: 'john@company.com',  
+    phone: '555-111-2222',  
+  },
+
+  { id: 'jane',
+    name: 'Jane doe',
+    position: 'system designer',            
+    email: 'amy@company.com',  
+    phone: '555-333-4444', 
+    bio: 'Leads engineering and R&D.' 
+  },
+
+  { id: 'jake', 
+    name: 'jake q', 
+    position: 'level designer',
+    email: 'mike@company.com', 
+    phone: '555-555-6666', 
+    bio: 'Keeps projects on time.' 
+  },
+
+  { id: 'emma', 
+    name: 'emma brown', 
+    position: 'graphics programmer',
+    email: 'mike@company.com', 
+    phone: '555-555-6666', 
+    bio: 'Keeps projects on time.' 
+  },
+
+  { id: 'rich', 
+    name: 'richard jones', 
+    position: 'audio programmer',
+    email: 'mike@company.com', 
+    phone: '555-555-6666', 
+    bio: 'Keeps projects on time.' 
+  }
+];
+
+
+app.get('/staff', (req, res) => {
+  res.render('staff/index', { staff });
+});
+
+
+app.get('/staff/:id', (req, res) => {
+  const employee = staff.find(employee => employee.id === req.params.id);
+  res.render('staff/show', { employee });
+});
+
 
 
 app.get('/', (req, res) => res.redirect('/links'));
 
 app.get('/links', (req, res) => {
-  res.render('links/index.ejs', { extLinks });
+  res.render('links/index', { extLinks });
 });
 
 app.get('/links/:id', (req, res) => {
   const link = extLinks.find(link => link.id === req.params.id);
-  res.render('links/show.ejs', { link });
+  res.render('links/show', { link });
 });
 
 
+
 app.listen(3000, () => {
-  console.log(`listen on port 3000`)
-})
+  console.log('listen on port 3000');
+});
